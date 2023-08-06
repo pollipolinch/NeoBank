fe7cbd376218ac8bd89b3d19988ca43309586ce0
pollipolinch <pollifriend@mail.ru>
2023-08-06 19:01:42 +0400

    feat: add features section

---
+++ src/components/mainpage/features/Features.js
@@ -0,0 +1,27 @@
+const features = document.querySelector('#features');
+import human from '../../../assets/img/human.png';
+import './features.css'
+
+
+class Features {
+    show() {
+        let template = `
+        <section class="features">
+        <img class="features__img" src=${human} alt="human">
+        <div class="features__info">
+            <h2 class="features__title">We Provide Many Features You Can Use</h2>
+            <p class="features__text">You can explore the features that we provide with fun and have their own
+                functions each feature</p>
+            <ul class="features__list">
+                <li class="features__list_item">Powerfull online protection.</li>
+                <li class="features__list_item">Cashback without borders.</li>
+                <li class="features__list_item">Personal design</li>
+                <li class="features__list_item">Work anywhere in the world</li>
+            </ul>
+        </div>
+    </section>
+        `;
+        features.innerHTML = template;
+    }
+}
+export default new Features();
--- src/script.js
+++ src/script.js
@@ -1,7 +1,9 @@
 import Header from "./components/header/Header";
 import Choose from "./components/mainpage/choose/Choose";
+import Features from "./components/mainpage/features/Features";
 
 ( () => {
 	Header.show()
     Choose.show()
+    Features.show()
 })()
