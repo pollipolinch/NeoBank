cd070c4673939d11e9945f3c1e8b0b0c055d3059
pollipolinch <pollifriend@mail.ru>
2023-08-06 18:52:23 +0400

    feat: add Header

---
+++ src/components/header/Header.js
@@ -0,0 +1,30 @@
+const header = document.querySelector('#header');
+import './header.css'
+
+class Header {
+    show() {
+        let template = `
+        <header class="header">
+        <a href="#" class="header__logo">NeoBank</a>
+        <input id="header__toggle" type="checkbox" />
+        <label class="header__hamb" for="header__toggle">
+            <span class="header__hamb_bar"></span>
+            <span class="header__hamb_bar"></span>
+            <span class="header__hamb_bar"></span>
+            <span class="header__hamb_bar"></span>
+        </label>
+        <nav class="header__nav">
+            <ul class="header__nav_list">
+                <li><a class="header__nav_link" href="#">Credit card</a></li>
+                <li><a class="header__nav_link" href="#">Product</a></li>
+                <li><a class="header__nav_link" href="#">Account</a></li>
+                <li><a class="header__nav_link" href="#">Resources</a></li>
+            </ul>
+        </nav>
+        <a href="#" class="header__button">Online Bank</a>
+    </header>
+        `;
+        header.innerHTML = template
+    }
+}
+export default new Header();
